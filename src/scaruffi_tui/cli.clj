e12102ec11431511f235bf41d6c5e7d632c75ecc
mBaratta96 <marcobaratta96@gmail.com>
2023-03-29 13:50:01 +0200

    format page

--- src/scaruffi_tui/cli.clj
+++ src/scaruffi_tui/cli.clj
@@ -10,14 +10,22 @@
       (cond (not (and (integer? in) (< in row-length))) (recur)
             :else in))))
 
+(defn get-page-headers
+  [page]
+  (filter #(or (= :h4 (:tag %)) (= :i (:tag %))) page))
+
 (defn get-upper-bound [bounds el] (last (filter #(< % el) bounds)))
 
 (defn create-section
   [page]
-  (let [bounds (map #(.indexOf page %)
-                    (filter #(or (= :h4 (:tag %)) (= :i (:tag %))) page))]
-    (partition-by #(get-upper-bound bounds (.indexOf page %))
-                  (filter #(= :p (:tag %)) page))))
+  (let [bounds (map #(.indexOf page %) (get-page-headers page))
+        paragraphs (filter #(and (= :p (:tag %))
+                                 (some? (:content %))
+                                 (> (count (:content %)) 1))
+                           page)]
+    ;;(println bounds)
+    ;;(println (map #(get-upper-bound bounds (.indexOf page %)) paragraphs))
+    (partition-by #(get-upper-bound bounds (.indexOf page %)) paragraphs)))
 
 (defn get-own-text
   [el]
@@ -35,8 +43,16 @@
                                               (string/upper-case
                                                (first (:content %)))
                                               :else "\n\n")
-                              (= % "\n") "\n"
                               :else (string/trim (string/replace % "\n" " ")))
                        content)))))
 
 (defn clear-console [] (print "\033\143"))
+
+(defn print-header
+  [header]
+  (println (string/upper-case (get-own-text header)) "\n"))
+
+(defn print-options
+  [rows]
+  (doseq [[i row] (map-indexed vector rows)]
+    (println (format "%d. %s" i (get-own-text row)))))
--- src/scaruffi_tui/core.clj
+++ src/scaruffi_tui/core.clj
@@ -3,35 +3,29 @@
   (:require [scaruffi-tui.scraper :as scraper]
             [scaruffi-tui.cli :as cli]))
 
-(defn print-options
-  [rows]
-  (doseq [[i row] (map-indexed vector rows)]
-    (println (format "%d. %s" i (cli/get-own-text row)))))
-
 (defn navigate-home
   []
   (let [table (scraper/get-table)
         rows (scraper/get-section-headers table)
         sections (scraper/get-section-content rows)]
-    (print-options rows)
+    (cli/print-options rows)
     (let [section (nth sections (cli/check-input (count rows)))]
       (cli/clear-console)
-      (print-options section)
+      (cli/print-options section)
       (let [chapter (nth section (cli/check-input (count section)))]
         (cli/clear-console)
         (scraper/get-link chapter)))))
 
+(defn navigate-page
+  [page]
+  (let [table (scraper/get-table page)
+        indexes (scraper/get-dir-indexes table)
+        headers (cli/get-page-headers indexes)
+        sections (cli/create-section indexes)]
+    (doseq [[i paragraphs] (map-indexed vector sections)]
+      (cli/print-header (nth headers i))
+      (doseq [paragraph paragraphs]
+        (println (cli/get-internal-text paragraph) "\n")))))
+
 (defn -main
-  ([]
-   (let [chapter-page (navigate-home)
-         table (scraper/get-table chapter-page)
-         ; headers (scraper/get-chapter-headers table)
-         indexes (scraper/get-dir-indexes table)]
-     ;(print-options headers)
-     (doseq [[i paragraphs] (map-indexed vector (cli/create-section indexes))]
-       (println "SECTION:" i)
-       (doseq [paragraph paragraphs]
-         (println (cli/get-internal-text paragraph) "\n"))))))
-       ;(println "PARAGRAPHS:" paragraphs)
-       ;(println (reduce #(str %1 (scraper/get-own-text %2)) ""
-       ;paragraphs))))))
+  ([] (let [chapter-page (navigate-home)] (navigate-page chapter-page))))
